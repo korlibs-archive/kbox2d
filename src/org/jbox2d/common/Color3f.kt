6bd5f510d36ddd01e9cf68dc87433f7cde3f460d
soywiz <soywiz@gmail.com>
2018-12-21 18:42:06 +0100

    Some conversions

--- src/org/jbox2d/common/Color3f.kt
+++ src/org/jbox2d/common/Color3f.kt
@@ -1,15 +1,15 @@
 /*******************************************************************************
  * Copyright (c) 2013, Daniel Murphy
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 	* Redistributions of source code must retain the above copyright notice,
- * 	  this list of conditions and the following disclaimer.
- * 	* Redistributions in binary form must reproduce the above copyright notice,
- * 	  this list of conditions and the following disclaimer in the documentation
- * 	  and/or other materials provided with the distribution.
- * 
+ * * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -20,21 +20,21 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- ******************************************************************************/
+ */
 /*
  * JBox2D - A Java Port of Erin Catto's Box2D
- * 
- * JBox2D homepage: http://jbox2d.sourceforge.net/ 
+ *
+ * JBox2D homepage: http://jbox2d.sourceforge.net/
  * Box2D homepage: http://www.box2d.org
- * 
+ *
  * This software is provided 'as-is', without any express or implied
  * warranty.  In no event will the authors be held liable for any damages
  * arising from the use of this software.
- * 
+ *
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
- * 
+ *
  * 1. The origin of this software must not be misrepresented; you must not
  * claim that you wrote the original software. If you use this software
  * in a product, an acknowledgment in the product documentation would be
@@ -44,45 +44,58 @@
  * 3. This notice may not be removed or altered from any source distribution.
  */
 
-package org.jbox2d.common;
+package org.jbox2d.common
 
 // updated to rev 100
 /**
  * Similar to javax.vecmath.Color3f holder
  * @author ewjordan
- *
  */
-public class Color3f {
-	
-	public static final Color3f WHITE = new Color3f(1, 1, 1);
-	public static final Color3f BLACK = new Color3f(0, 0, 0);
-	public static final Color3f BLUE = new Color3f(0, 0, 1);
-	public static final Color3f GREEN = new Color3f(0, 1, 0);
-	public static final Color3f RED = new Color3f(1, 0, 0);
-	
-	public float x;
-	public float y;
-	public float z;
+class Color3f {
 
-	
-	public Color3f(){
-		x = y = z = 0;
-	}
-	public Color3f(float r, float g, float b) {
-		x = r;
-		y = g;
-		z = b;
-	}
-	
-	public void set(float r, float g, float b){
-		x = r;
-		y = g;
-		z = b;
-	}
-	
-	public void set(Color3f argColor){
-		x = argColor.x;
-		y = argColor.y;
-		z = argColor.z;
-	}
+    @JvmField
+    var x: Float = 0.toFloat()
+    @JvmField
+    var y: Float = 0.toFloat()
+    @JvmField
+    var z: Float = 0.toFloat()
+
+
+    constructor() {
+        z = 0f
+        y = z
+        x = y
+    }
+
+    constructor(r: Float, g: Float, b: Float) {
+        x = r
+        y = g
+        z = b
+    }
+
+    operator fun set(r: Float, g: Float, b: Float) {
+        x = r
+        y = g
+        z = b
+    }
+
+    fun set(argColor: Color3f) {
+        x = argColor.x
+        y = argColor.y
+        z = argColor.z
+    }
+
+    companion object {
+
+        @JvmField
+        val WHITE = Color3f(1f, 1f, 1f)
+        @JvmField
+        val BLACK = Color3f(0f, 0f, 0f)
+        @JvmField
+        val BLUE = Color3f(0f, 0f, 1f)
+        @JvmField
+        val GREEN = Color3f(0f, 1f, 0f)
+        @JvmField
+        val RED = Color3f(1f, 0f, 0f)
+    }
 }
