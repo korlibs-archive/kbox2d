c752e83915683492fe2e9ffcca860caea1f3415c
soywiz <soywiz@gmail.com>
2018-12-21 18:36:16 +0100

    Some interfaces conversion

--- src/org/jbox2d/dynamics/contacts/ContactCreator.kt
+++ src/org/jbox2d/dynamics/contacts/ContactCreator.kt
@@ -1,15 +1,15 @@
 /*******************************************************************************
  * Copyright (c) 2013, Daniel Murphy
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 	* Redistributions of source code must retain the above copyright notice,
- * 	  this list of conditions and the following disclaimer.
- * 	* Redistributions in binary form must reproduce the above copyright notice,
- * 	  this list of conditions and the following disclaimer in the documentation
- * 	  and/or other materials provided with the distribution.
- * 
+ * * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -20,16 +20,16 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- ******************************************************************************/
-package org.jbox2d.dynamics.contacts;
+ */
+package org.jbox2d.dynamics.contacts
 
-import org.jbox2d.dynamics.Fixture;
-import org.jbox2d.pooling.IWorldPool;
+import org.jbox2d.dynamics.Fixture
+import org.jbox2d.pooling.IWorldPool
 
 // updated to rev 100
-public interface ContactCreator {
+interface ContactCreator {
 
-	public Contact contactCreateFcn(IWorldPool argPool, Fixture fixtureA, Fixture fixtureB);
-	
-	public void contactDestroyFcn(IWorldPool argPool, Contact contact);
+    fun contactCreateFcn(argPool: IWorldPool, fixtureA: Fixture, fixtureB: Fixture): Contact
+
+    fun contactDestroyFcn(argPool: IWorldPool, contact: Contact)
 }
