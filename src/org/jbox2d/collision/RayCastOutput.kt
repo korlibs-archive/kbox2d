56043dce0ece9696931277cf75e5f33c4b314f36
soywiz <soywiz@gmail.com>
2018-12-21 19:26:12 +0100

    Some conversions

--- src/org/jbox2d/collision/RayCastOutput.kt
+++ src/org/jbox2d/collision/RayCastOutput.kt
@@ -1,15 +1,15 @@
 /*******************************************************************************
  * Copyright (c) 2013, Daniel Murphy
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 	* Redistributions of source code must retain the above copyright notice,
- * 	  this list of conditions and the following disclaimer.
- * 	* Redistributions in binary form must reproduce the above copyright notice,
- * 	  this list of conditions and the following disclaimer in the documentation
- * 	  and/or other materials provided with the distribution.
- * 
+ * * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -20,27 +20,29 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- ******************************************************************************/
-package org.jbox2d.collision;
+ */
+package org.jbox2d.collision
 
-import org.jbox2d.common.Vec2;
+import org.jbox2d.common.Vec2
 
 // updated to rev 100
 /**
  * Ray-cast output data. The ray hits at p1 + fraction * (p2 - p1), where p1 and p2
  * come from b2RayCastInput.
  */
-public class RayCastOutput{
-	public final Vec2 normal;
-	public float fraction;
+class RayCastOutput {
+    @JvmField
+    val normal: Vec2
+    @JvmField
+    var fraction: Float = 0.toFloat()
 
-	public RayCastOutput(){
-		normal = new Vec2();
-		fraction = 0;
-	}
+    init {
+        normal = Vec2()
+        fraction = 0f
+    }
 
-	public void set(final RayCastOutput rco){
-		normal.set(rco.normal);
-		fraction = rco.fraction;
-	}
-};
+    fun set(rco: RayCastOutput) {
+        normal.set(rco.normal)
+        fraction = rco.fraction
+    }
+}
