d45e572e718fd88d3a1e3a9f3e5ebf07b847bb12
soywiz <soywiz@gmail.com>
2018-12-21 18:51:24 +0100

    Some conversions

--- src/org/jbox2d/dynamics/contacts/Position.kt
+++ src/org/jbox2d/dynamics/contacts/Position.kt
@@ -1,15 +1,15 @@
 /*******************************************************************************
  * Copyright (c) 2013, Daniel Murphy
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 	* Redistributions of source code must retain the above copyright notice,
- * 	  this list of conditions and the following disclaimer.
- * 	* Redistributions in binary form must reproduce the above copyright notice,
- * 	  this list of conditions and the following disclaimer in the documentation
- * 	  and/or other materials provided with the distribution.
- * 
+ * * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -20,12 +20,14 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- ******************************************************************************/
-package org.jbox2d.dynamics.contacts;
+ */
+package org.jbox2d.dynamics.contacts
 
-import org.jbox2d.common.Vec2;
+import org.jbox2d.common.Vec2
 
-public class Position {
-  public final Vec2 c = new Vec2();
-  public float a;
+class Position {
+    @JvmField
+    val c = Vec2()
+    @JvmField
+    var a: Float = 0.toFloat()
 }
