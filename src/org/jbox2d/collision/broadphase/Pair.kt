f7de1cb74ff596c61d0bc610cce926ac2d3327cf
soywiz <soywiz@gmail.com>
2018-12-21 18:48:49 +0100

    Some conversions

--- src/org/jbox2d/collision/broadphase/Pair.kt
+++ src/org/jbox2d/collision/broadphase/Pair.kt
@@ -1,15 +1,15 @@
 /*******************************************************************************
  * Copyright (c) 2013, Daniel Murphy
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 	* Redistributions of source code must retain the above copyright notice,
- * 	  this list of conditions and the following disclaimer.
- * 	* Redistributions in binary form must reproduce the above copyright notice,
- * 	  this list of conditions and the following disclaimer in the documentation
- * 	  and/or other materials provided with the distribution.
- * 
+ * * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -20,27 +20,28 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- ******************************************************************************/
-package org.jbox2d.collision.broadphase;
+ */
+package org.jbox2d.collision.broadphase
 
 // updated to rev 100
 /**
  * Java note: at the "creation" of each node, a random key is given to that node, and that's what we
  * sort from.
  */
-public class Pair implements Comparable<Pair> {
-  public int proxyIdA;
-  public int proxyIdB;
+class Pair : Comparable<Pair> {
+    @JvmField
+    var proxyIdA: Int = 0
+    @JvmField
+    var proxyIdB: Int = 0
 
-  public int compareTo(Pair pair2) {
-    if (this.proxyIdA < pair2.proxyIdA) {
-      return -1;
+    override fun compareTo(pair2: Pair): Int {
+        if (this.proxyIdA < pair2.proxyIdA) {
+            return -1
+        }
+
+        return if (this.proxyIdA == pair2.proxyIdA) {
+            if (proxyIdB < pair2.proxyIdB) -1 else if (proxyIdB == pair2.proxyIdB) 0 else 1
+        } else 1
+
     }
-
-    if (this.proxyIdA == pair2.proxyIdA) {
-      return proxyIdB < pair2.proxyIdB ? -1 : proxyIdB == pair2.proxyIdB ? 0 : 1;
-    }
-
-    return 1;
-  }
 }
