da872a776d998bd2f61c1927f149f2c9b4fc3a9e
soywiz <soywiz@gmail.com>
2019-01-18 10:12:10 +0100

    Support null values to the typed userdata

--- src/nativeCommonMain/kotlin/org/jbox2d/internal/System_nanoTime.kt
+++ src/nativeCommonMain/kotlin/org/jbox2d/internal/System_nanoTime.kt
@@ -1,4 +1,4 @@
 package org.jbox2d.internal
 
 actual fun System_nanoTime(): Long = kotlin.system.getTimeNanos()
-actual typealias NativeThreadLocal = ThreadLocal
+actual typealias NativeThreadLocal = kotlin.native.concurrent.ThreadLocal
